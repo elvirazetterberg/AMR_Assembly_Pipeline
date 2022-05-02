fa484dcf29443cf841dc514e88080d0e75db9148
Evelina Andersson <evelina.andersson.2885@student.uu.se>
2022-05-02 10:28:31 +0200

    Report to csv attempt

--- assembly_pipeline_v3_1.py
+++ assembly_pipeline_v3_1.py
@@ -213,13 +213,15 @@ def main():
             # Add later lol --threads {threads_available}
             # in the old pipeline they have two names for the input (fastqfile1 and filename1_short) and I do not know why
             #krakeninput = f'kraken2 --db {path_kraken} --output output_test.kraken.out --report output_test.kraken.report --paired {infile1} {infile2}'
-            krakeninput = f'kraken2 --db {path_kraken} --output out_kraken.csv --report report_kraken.csv --paired {infile1} {infile2}'
+            krakeninput = f'kraken2 --db {path_kraken} --output out_kraken.out --report report_kraken.report --paired {infile1} {infile2}'
             os.system(krakeninput)
 
             time = currenttime()
             log.writelines(time)
 
-            with open('report_kraken.csv', 'rb') as kraken_report: # add "or die" or similar? see old pipeline
+            csv_kraken = r"kraken_report.csv"
+
+            with open(report_kraken.report, 'r') as kraken_report: # add "or die" or similar? see old pipeline
             #    result_reader = csv.reader(kraken_report, delimiter=' ')
             #   # result_reader.next()
             #    for row in result_reader:
@@ -227,7 +229,13 @@ def main():
             #            columns[i].append(v)
             #print(columns[0])
 
-                df = pd.read_csv(kraken_report, header=None)
+            #with open(txt_file, "r") as in_text:
+                out_filename= 'kraken_report.csv'
+                
+                df = pd.read_csv(kraken_report, sep="/t")
+                df.to_csv(out_filename, index=False)
+
+                df = pd.read_csv(out_filename, header=None)
                 #Due to python indexing, the enumerating of the columns starts at 0
                 column1 = df.iloc[:,0]
                 column3 = df.iloc[:,2]
