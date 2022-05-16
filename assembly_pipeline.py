1f22a563f7458aa3585d2c0a2fb6c681537961b8
elvirazetterberg <elvira.zetterberg@gmail.com>
2022-05-16 16:52:33 +0200

    :)

--- assembly_pipeline.py
+++ assembly_pipeline.py
@@ -568,7 +568,7 @@ def parallelize(finalpath, file_directory):
     all_filenames = [i for i in glob.glob(f'{infopath}/*/*info.csv')]  
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ], axis=0) 
     combined_csv["Genome Name"] = com_names # EDITED: from appending names in loop above
-    combined_csv.to_csv( f'{infopath}/{finalname}.csv', index=False, encoding='utf-8-sig')
+    combined_csv.to_csv(path_or_buf= f'{infopath}/{finalname}.csv', index=False, encoding='utf-8-sig')
     
 
 def main():
